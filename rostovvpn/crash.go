5075a5dd3c5cfa77bf528f95514d894e8738fb2d
Darkmen203 <dima-pivovarov03@mail.ru>
2025-09-15 01:29:37 +0300

    fix name package

---
+++ rostovvpn/crash.go
@@ -0,0 +1,19 @@
+package rostovvpn
+
+import (
+    "fmt"
+    "runtime/debug"
+)
+
+func CrashHandle(f func()) {
+    defer NoCrash()
+    f()
+}
+
+func NoCrash() {
+    if r := recover(); r != nil {
+        fmt.Println("Wireguard error!", r, string(debug.Stack()))
+    }
+
+}
+
